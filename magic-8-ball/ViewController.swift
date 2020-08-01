e814bd8fe9c9235a9f4db4bb1e474959eb94335e
guidoasbun <gdrodrigo8@gmail.com>
2020-07-31 21:55:27 -0700

    Initial Commit

---
+++ magic-8-ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic-8-ball
+//
+//  Created by Rodrigo Asbun on 7/31/20.
+//  Copyright © 2020 Guido Asbun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
